fa1dcee0dea83d03723a807ef52654a29b3a9f2b
amboob223 <monk8896@gmail.com>
2024-01-09 04:53:08 -0500

    spin

--- src/contract.sol
+++ src/contract.sol
@@ -4,24 +4,41 @@ pragma solidity ^0.8.18;
 
 contract xalis {
     address public owner;
-    string[] public reviews; // Declare an array to store reviews
+
+    address[] public addresses;
+
 
     struct Person {
         address reviewer;
-        string review;
+        string[] reviews;
     }
 
     mapping(address => Person) public people;
     uint public reviewId;
 
-    function addReview(string memory _review) public {
-        Person storage newPerson = people[msg.sender];
-        newPerson.review = _review;
-        reviews.push(_review); // Add the review to the array
-        reviewId++;
+
+    function addPerson(address  _address, string memory _review ) public{
+        Person storage newPerson = people[_address];
+            addresses.push(_address);
+            newPerson.reviews.push(_review);
+            newPerson.reviewer = _address;
     }
 
-    function getAll() public view returns (string[] memory){
-        return reviews;
+    function addReview(address _address, string memory _review) public {
+        Person storage newPerson = people[_address];
+        newPerson.reviews.push(_review);
+    }
+
+
+
+    function getAll() public view returns (address[] memory){
+        return addresses;
+    }
+    function getAllReviews(address _address) public view returns(string[][] memory){
+       string[][] memory AllReviews = new string[][](1);
+       AllReviews[0] = people[_address].reviews;
+       return AllReviews;
+
     }
 }
+ 
